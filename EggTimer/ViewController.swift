292b1d0e06c1a1f362ad42ed8c30757e498b9ad6
itzVort3x1 <saikaustubh10@gmail.com>
2022-06-26 10:20:21 +0530

    initial commit

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
+    
+    let eggTimes: [String: Int] = ["Soft": 320, "Medium": 420, "Hard": 720];
+    @IBOutlet weak var headerText: UILabel!
+    
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    var timer = Timer()
+    
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
+        progressBar.progress = 0.0;
+        
+        let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        
+        headerText.text = "How do you like your eggs?"
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    @objc func updateTimer(){
+        if secondsPassed < totalTime{
+            
+            progressBar.progress = Float(secondsPassed) / Float(totalTime);
+            
+            secondsPassed += 1;
+        }else {
+            headerText.text = "DONE!";
+            progressBar.progress = 1.0;
+            playSound();
+        }
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Egg Timer App using Swift
+
+This is a simple app that can help you cook your egg to perfection based on how you like it. I built this to learn the basics of swift. How to play audio, How to update timer and How to Update text on the screen.
+
+### #ScreenShot 1
+<img src="./ss1.png" alt="screenShot1" height="512px"></img>
+
+### #ScreenShot 2
+<img src="./ss2.png" alt="screenShot2" height="512px"></img>
